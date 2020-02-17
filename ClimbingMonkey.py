8f06b2d5dad77af0e1036029908e3d17d7a59bdd
SamuelSennero <samuel.sennero@gmail.com>
2020-02-17 10:03:35 +0100

    window

--- ClimbingMonkey.py
+++ ClimbingMonkey.py
@@ -1,1 +1,36 @@
-#Beginning of an era
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+pygame.init()
+
+size = (400, 600)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("Climbing Monkey")
+ 
+done = False
+ 
+clock = pygame.time.Clock()
+ 
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+ 
+
+
+    screen.fill(GREEN)
+ 
+
+    pygame.display.flip()
+ 
+
+    clock.tick(60)
+ 
+
+pygame.quit()
