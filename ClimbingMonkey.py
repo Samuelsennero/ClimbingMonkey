22da18a968b22927043fd8837fd37b782bb20806
SamuelSennero <samuel.sennero@gmail.com>
2020-04-22 23:16:12 +0200

    music

--- ClimbingMonkey.py
+++ ClimbingMonkey.py
@@ -9,10 +9,11 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BROWN = (102, 51, 0)
 LIGHTBLUE = (153, 179, 255)
+LIGHT = (254, 251, 232)
 
 #Screen setup
 pygame.init()
-
+ 
 size = (400, 600)
 screen = pygame.display.set_mode(size)
 
@@ -67,12 +68,17 @@ f = 0
 
 #Main game loop
 while not done:
+    
+    #Music
+    pygame.mixer.music.load('Our-Mountain_v003.mp3')
+    pygame.mixer.music.play(-1)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                if flipped == False:
+                if flipped == False: 
                     monkeyflip = monkeyflipImg
                     monkey_x += 160
                     flipped = True
@@ -80,6 +86,7 @@ while not done:
                     monkeyflip = monkeyImg
                     monkey_x -= 160
                     flipped = False
+                    
 
               
     while menu == True:
@@ -92,7 +99,7 @@ while not done:
              if event.button == 1:
                     mousepos = pygame.mouse.get_pos()
              if 380 >= mousepos[0] >= 250 and 400 >= mousepos[1] >= 330 and menuscreen == menuImg:
-                    menu = False
+                    menu = False    
              elif 130 >= mousepos[0] >= 0 and 370 >= mousepos[1] >= 315:
                     menuscreen = howtoplayImg
              elif 385 >= mousepos[0] >= 260 and 405 >= mousepos[1] >= 345 and menuscreen == howtoplayImg:
@@ -103,11 +110,6 @@ while not done:
     
     #Animations and spawns
     screen.fill(LIGHTBLUE)
-
-
-    #Music
-    #pygame.mixer.music.load()
-    #pygame.mixer.music.play(-1)
     
 
     #Branch randomizer
@@ -164,8 +166,6 @@ while not done:
     y += 3
 
     screen.blit(monkeyflip,(monkey_x,monkey_y))
-    
- 
 
 
     pygame.display.flip()
